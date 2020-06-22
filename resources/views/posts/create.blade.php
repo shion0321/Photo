fe4058a0d390dda7f8616c9204440b1022db4c2a
Shion Aoki <shio0321pc@gmail.com>
2020-06-22 23:56:52 +0900

    add

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,9 +1,11 @@
 <h1>新規登録</h1>
 <a href="{{ route('posts.index') }}">一覧に戻る</a>
-<form action="{{ route('posts.store') }}" method="POST">
+<form action="{{ route('posts.store') }}" method="POST" enctype="multipart/form-data">
 	@csrf
 	<!-- フォームの説明 -->
 	<label for="desctiption">内容</label>
 	<input type="text" name="description" id="description">
+	<label for="photo_image">写真</label>
+	<input type="file" name="photo_image"> 
 	<button>送信</button>
 </form>
