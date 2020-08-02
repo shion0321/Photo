f8f3133a05d69d5f9b29debd5c969b5b9252c85b
Shion Aoki <shio0321pc@gmail.com>
2020-08-02 15:27:04 +0900

    add

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,11 +1,21 @@
-<h1>新規登録</h1>
-<a href="{{ route('posts.index') }}">一覧に戻る</a>
-<form action="{{ route('posts.store') }}" method="POST" enctype="multipart/form-data">
-	@csrf
-	<!-- フォームの説明 -->
-	<label for="desctiption">内容</label>
-	<input type="text" name="description" id="description">
-	<label for="photo_image">写真</label>
-	<input type="file" name="photo_image"> 
-	<button>送信</button>
-</form>
+@extends('layouts.app')
+@section('content')
+	<h1>新規登録</h1>
+	<a href="{{ route('posts.index') }}">一覧に戻る</a>
+	<form action="{{ route('posts.store') }}" method="POST" enctype="multipart/form-data">
+		@csrf
+		<div>
+			<label for="desctiption">内容</label>
+			<input type="text" name="description" id="description">
+		</div>
+		<div>
+			<label for="photo_image">写真</label>
+			<input type="file" name="photo_image"> 
+		</div>
+		<div class="form-group">
+			<label for="tags">タグ</label>
+			<input type="text" name="tags" id="" class="form-control">
+		</div>
+		<button>送信</button>
+	</form>
+@endsection
